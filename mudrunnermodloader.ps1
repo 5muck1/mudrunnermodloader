0976d8474680a898a28197651d77d3eaea67e258
smuckl <33559818+smuckl@users.noreply.github.com>
2017-12-20 15:26:22 +0100

    Add files via upload

---
+++ mudrunnermodloader.ps1
@@ -0,0 +1,10 @@
+﻿[String]$mud = <GameDirectory>
+[String]$mods = <ModDirectory>
+
+cd $mud
+rm Media -r -fo
+New-Item -ItemType directory -Path Media
+.\mudrunner.exe
+sleep 10
+
+Copy-Item "$mods\*" Media -Recurse
